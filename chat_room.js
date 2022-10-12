3ba5c0c9f36054e61cc33f64e9045cd0c0ae04da
Evan <106592768+EvanH1227@users.noreply.github.com>
2022-10-12 18:25:10 -0500

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGBOlZVeko0pd5YFHiS-bnHQcXhw9Toac",
+    authDomain: "let-s-chat-edfd6.firebaseapp.com",
+    projectId: "let-s-chat-edfd6",
+    storageBucket: "let-s-chat-edfd6.appspot.com",
+    messagingSenderId: "946532705596",
+    appId: "1:946532705596:web:cc0e9124c8d418015faa39",
+    measurementId: "G-D197WRMXB2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
